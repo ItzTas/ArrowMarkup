9191140c5e42fc56ffa8ba24ad8f0357ee1e01d5
ItzTas <talessabini2@gmail.com>
2024-08-11 02:30:18 -0400

    fix: fixes in white spaces

--- am_parser.go
+++ am_parser.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"errors"
-	"regexp"
 	"strings"
-
-	"github.com/ItzTas/arrowmarkup/internal/models"
 )
 
 const (
@@ -16,61 +13,47 @@ var (
 	errorOutOfRange = errors.New("Text out of range")
 )
 
-type AMParser struct {
-	regex *regexp.Regexp
-}
-
-func NewAmParser() *AMParser {
-	return &AMParser{
-		regex: regexp.MustCompile(`(<-\w+-)|(-\w+->)|([^<>-]+)`),
-	}
-}
-
-func (p *AMParser) parseAM(str string) (models.NodeAM, error) {
+func (p *AMParser) parseAM(str string) (NodeAM, error) {
 	str = strings.Trim(str, " ")
 	texts := p.regex.FindAllString(str, -1)
 	if len(texts) == 0 {
-		return models.NodeAM{}, errors.New("invalid string")
+		return NodeAM{}, errors.New("invalid string")
 	}
 	if len(texts) == 1 {
 		if p.isAM(texts[0]) {
-			return models.NodeAM{}, errorOutOfRange
+			return NodeAM{}, errorOutOfRange
 		}
-		return models.NodeAM{
-			Text:       texts[0],
-			Tag:        paragraph,
-			Attributes: make(map[string]string),
+		return NodeAM{
+			Text: strings.Trim(texts[0], " "),
+			Tag:  paragraph,
 		}, nil
 	}
 
 	for i, t := range texts {
 		if t[0] == '-' && t[len(t)-2:] == "->" && len(t) >= 3 {
 			if i+1 == len(texts) {
-				return models.NodeAM{}, errorOutOfRange
+				return NodeAM{}, errorOutOfRange
 			}
 			tag := parseTag(t)[0]
-			return models.NodeAM{
-				Text:       texts[i+1],
-				Tag:        tag,
-				Attributes: make(map[string]string),
+			return NodeAM{
+				Text: strings.Trim(texts[i+1], " "),
+				Tag:  tag,
 			}, nil
 		}
 		if t[:2] == "<-" && len(t) >= 3 {
 			if i-1 < 0 {
-				return models.NodeAM{}, errorOutOfRange
+				return NodeAM{}, errorOutOfRange
 			}
 			tag := parseTag(t)[0]
-			return models.NodeAM{
-				Text:       texts[i-1],
-				Tag:        tag,
-				Attributes: make(map[string]string),
+			return NodeAM{
+				Text: strings.Trim(texts[i-1], " "),
+				Tag:  tag,
 			}, nil
 		}
 	}
-	return models.NodeAM{
-		Text:       str,
-		Tag:        paragraph,
-		Attributes: make(map[string]string),
+	return NodeAM{
+		Text: strings.Trim(str, " "),
+		Tag:  paragraph,
 	}, nil
 }
 
@@ -79,10 +62,13 @@ func (p *AMParser) isAM(str string) bool {
 }
 
 func parseTag(str string) []string {
+	str = strings.Trim(str, " ")
 	if str[0] == '<' {
 		s := str[2 : len(str)-1]
+		s = strings.Trim(s, " ")
 		return strings.Split(s, " ")
 	}
 	s := str[1 : len(str)-2]
+	s = strings.Trim(s, " ")
 	return strings.Split(s, " ")
 }
