5edc993d2e5e60bd0bb7deefe70964a611cb179b
ItzTas <talessabini2@gmail.com>
2024-08-16 18:00:50 -0400

    chore: everything cleaned

--- main.go
+++ main.go
@@ -1,14 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"regexp"
-)
-
 func main() {
-	text := ".class.< testclass anotherclass > .href.< testLink >"
-	regex := regexp.MustCompile(`\.(\w+)\.<([^>]+)>`)
-
-	matches := regex.FindAllString(text, -1)
-	fmt.Println(parseAttributes(matches))
 }
